0cf5859fc0d6ab4eee4b45b6c30e445c3bdf1ee7
Jiatao Liang <jtliang24@gmail.com>
2026-02-13 23:13:09 -0500

    repin to nixos-unstable after fix has been updated

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?ref=pull/488420/head";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     nvf.url = "github:notashelf/nvf";
   };
