879c8ca624a7aedd074975170657b6deecafeeca
Jiatao Liang <jtliang24@gmail.com>
2026-04-01 23:30:22 -0400

    unwrapped artix games launcher

--- Artix_Game_Launcher.nix
+++ Artix_Game_Launcher.nix
@@ -1,8 +1,8 @@
 {
-  appimageTools,
-  stdenv,
-  fetchurl,
   lib,
+  fetchurl,
+  makeWrapper,
+  stdenvNoCC,
   ...
 }:
 let
@@ -12,12 +12,37 @@ let
     url = "https://launch.artix.com/latest/Artix_Games_Launcher-x86_64.AppImage";
     sha256 = "sha256-8eVXOm5g92wErWa6lbTXrCL04MWYlObjonHJk+oUI3E=";
   };
-  appimageContents = appimageTools.extract {
-    inherit pname version src;
-  };
 in
-appimageTools.wrapType2 {
+stdenvNoCC.mkDerivation {
   inherit pname version src;
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  dontUnpack = true;
+
+  installPhase = ''
+    runHook preInstall
+
+    install -Dm755 $src $out/libexec/ArtixGameLauncher.AppImage
+
+    # The AppImage bundles its own runtime and must use the host's glibc/ld-linux,
+    # so we run it directly rather than through an FHS sandbox.
+    makeWrapper $out/libexec/ArtixGameLauncher.AppImage $out/bin/${pname} \
+      --append-flags "--no-sandbox"
+
+    mkdir -p $out/share/applications $out/share/icons
+    cat > $out/share/applications/ArtixGamesLauncher.desktop << EOF
+    [Desktop Entry]
+    Name=Artix Game Launcher
+    Exec=${pname} %u
+    Icon=ArtixLogo
+    Type=Application
+    Categories=Game;
+    MimeType=x-scheme-handler/artix;
+    EOF
+
+    runHook postInstall
+  '';
 
   meta = with lib; {
     description = "The unofficial Nix packaging for the official Artix Game Launcher.";
@@ -25,14 +50,6 @@ appimageTools.wrapType2 {
     license = licenses.unfree;
     maintainers = [ "jtliang24" ];
     platforms = [ "x86_64-linux" ];
+    sourceProvenance = with sourceTypes; [ binaryNativeCode ];
   };
-
-  extraPkgs = pkgs: [ ];
-
-  extraInstallCommands = ''
-    mkdir -p $out/share/applications
-    install -m 444 -D ${appimageContents}/ArtixGamesLauncher.desktop $out/share/applications/ArtixGamesLauncher.desktop
-    install -m 444 -D ${appimageContents}/ArtixLogo.png $out/share/icons/ArtixLogo.png
-  '';
-
 }
