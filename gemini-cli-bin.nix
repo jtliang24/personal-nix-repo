8f52bc7274c59a8b068569a55b08a20c74b25a81
Jiatao Liang <jtliang24@gmail.com>
2026-04-13 18:03:59 -0400

    align with the nixpkgs gemini-cli-bin

--- gemini-cli-bin.nix
+++ gemini-cli-bin.nix
@@ -1,9 +1,8 @@
 {
   lib,
   stdenvNoCC,
-  fetchurl,
+  fetchzip,
   nodejs,
-  unzip,
   sysctl,
   writableTmpDirAsHomeHook,
   nix-update-script,
@@ -12,20 +11,18 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "gemini-cli-bin";
-  version = "0.37.1";
+  version = "0.37.0";
 
-  src = fetchurl {
+  src = fetchzip {
     url = "https://github.com/google-gemini/gemini-cli/releases/download/v${finalAttrs.version}/gemini-cli-bundle.zip";
-    hash = "sha256:sha256-iCjdf6HlsS0Kk/vNbs2tfz0VjYFJX6u7PwwCvHjko84=";
+    hash = "sha256-JbR37CZGjZN0px+bGLQ67dbo/yyAb7BSx1v6IVuBCJc=";
+    stripRoot = false;
   };
-
-  dontUnpack = true;
 
   strictDeps = true;
 
   nativeBuildInputs = [
     makeWrapper
-    unzip
   ];
 
   buildInputs = [
@@ -33,25 +30,41 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     ripgrep
   ];
 
+  patchPhase = ''
+    runHook prePatch
+
+    # chunk filenames contain unpredictable hashes, use glob to iterate
+    for chunk in ./chunk-*.js; do
+
+      # disable auto-update
+      if grep -q 'enableAutoUpdate: {' "$chunk"; then
+        sed -i '/enableAutoUpdate: {/,/}/ s/default: true/default: false/' "$chunk"
+      fi
+
+      # use `ripgrep` from `nixpkgs`, more dependencies but prevent downloading incompatible binary on NixOS
+      # this workaround can be removed once the following upstream issue is resolved:
+      # https://github.com/google-gemini/gemini-cli/issues/11438
+      if grep -q 'await resolveExistingRgPath();' "$chunk"; then
+        substituteInPlace "$chunk" \
+          --replace-fail 'const existingPath = await resolveExistingRgPath();' 'const existingPath = "${lib.getExe ripgrep}";'
+      fi
+
+    done
+
+    runHook postPatch
+  '';
+
   installPhase = ''
     runHook preInstall
 
-    mkdir -p "$out/lib/gemini"
-    unzip "$src" -d "$out/lib/gemini"
-    local dest="$out/lib/gemini/gemini.js"
+    local bundleDir="$out/lib/gemini"
 
-    # use `ripgrep` from `nixpkgs`, more dependencies but prevent downloading incompatible binary on NixOS
-    # this workaround can be removed once the following upstream issue is resolved:
-    # https://github.com/google-gemini/gemini-cli/issues/11438
-    for file in "$out/lib/gemini"/*.js; do
-      substituteInPlace "$file" \
-        --replace 'const existingPath = await resolveExistingRgPath();' 'const existingPath = "${lib.getExe ripgrep}";'
-      sed -i '/enableAutoUpdate: {/,/}/ s/default: true/default: false/' "$file"
-    done
+    mkdir -p "$bundleDir"
+    cp -aT . "$bundleDir"
 
     makeWrapper "${lib.getExe nodejs}" "$out/bin/gemini" \
       --add-flags "--no-warnings=DEP0040" \
-      --add-flags "$dest"
+      --add-flags "$bundleDir/gemini.js"
 
     runHook postInstall
   '';
@@ -63,17 +76,18 @@ stdenvNoCC.mkDerivation (finalAttrs: {
   ++ lib.optionals (with stdenvNoCC.hostPlatform; isDarwin && isx86_64) [
     sysctl
   ];
-  # versionCheckHook cannot be used because the reported version might be incorrect (e.g., 0.6.1 returns 0.6.0).
+
+  # versionCheckHook cannot be used because it assumes the executable is hermetic,
+  # but we need `nativeInstallCheckInputs`
   installCheckPhase = ''
     runHook preInstallCheck
 
-    "$out/bin/gemini" -v
+    "$out/bin/gemini" -v | grep "${finalAttrs.version}"
 
     runHook postInstallCheck
   '';
 
-  passthru.updateScript = nix-update-script {
-  };
+  passthru.updateScript = nix-update-script { };
 
   meta = {
     description = "AI agent that brings the power of Gemini directly into your terminal";
