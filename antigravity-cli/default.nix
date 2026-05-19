d5b4ebed5c4c0ad57732da8759aebd306bdf8af3
Jiatao Liang <jtliang24@gmail.com>
2026-05-19 17:58:59 -0400

    use nixpkgs source

--- antigravity-cli/default.nix
+++ antigravity-cli/default.nix
@@ -3,34 +3,35 @@
   stdenv,
   fetchurl,
   autoPatchelfHook,
-  zlib,
 }:
-let
-  pname = "antigravity-cli";
-  version = "1.0.0";
 
+let
   sources = lib.importJSON ./sources.json;
-  srcInfo =
-    sources.${stdenv.hostPlatform.system}
-      or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
 in
 stdenv.mkDerivation {
-  inherit pname version;
+  pname = "antigravity-cli";
+  inherit (sources) version;
+
+  strictDeps = true;
+  __structuredAttrs = true;
 
   src = fetchurl {
-    inherit (srcInfo) url hash;
+    inherit
+      (sources.sources.${stdenv.hostPlatform.system}
+        or (throw "Unsupported system: ${stdenv.hostPlatform.system}")
+      )
+      url
+      sha512
+      ;
   };
 
   sourceRoot = ".";
 
-  nativeBuildInputs = [
-    autoPatchelfHook
-  ];
-
-  buildInputs = [
-    zlib
-    stdenv.cc.cc.lib
-  ];
+  # The source archive is a tar.gz containing the single binary named 'antigravity'.
+  # The default unpackPhase will unpack it into the current directory.
+  # We copy 'antigravity' to $out/bin/agy and create a symlink named 'antigravity' to it.
+  # If running on macOS, we don't need any patching, but on Linux we need autoPatchelfHook.
+  nativeBuildInputs = lib.optionals stdenv.isLinux [ autoPatchelfHook ];
 
   installPhase = ''
     runHook preInstall
@@ -39,17 +40,21 @@ stdenv.mkDerivation {
     runHook postInstall
   '';
 
+  passthru.updateScript = ./update.sh;
+
   meta = with lib; {
-    description = "Official CLI for Antigravity";
+    description = "Antigravity CLI - A powerful tool for agentic workflows";
     homepage = "https://antigravity.google";
     license = licenses.unfree;
     platforms = [
       "x86_64-linux"
       "aarch64-linux"
+      "x86_64-darwin"
       "aarch64-darwin"
     ];
-    maintainers = [ ];
     mainProgram = "agy";
-    sourceProvenance = with sourceTypes; [ binaryNativeCode ];
+    maintainers = with maintainers; [
+      deftdawg
+    ];
   };
 }
