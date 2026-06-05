99fc99c7f0200cf10335cfd5942d8008afe47bf7
Jiatao Liang <jtliang24@gmail.com>
2026-06-05 15:26:14 -0400

    nix dev shell

---
+++ shell.nix
@@ -0,0 +1,17 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+
+pkgs.mkShell {
+  packages = with pkgs; [
+    # Repository Updates (used in update.sh)
+    nix-update # Automatically updates package version and hashes
+    jq # JSON parser for version lookup in updates
+    curl # Fetching API/release information
+
+    # Scripts and Workflows
+    shellcheck # Bash script linter for update.sh
+  ];
+
+  shellHook = "Activated nix development shell";
+}
