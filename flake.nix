4667c970f55668fe0786ae4893a1bf05105faaf3
Jiatao Liang <jtliang24@gmail.com>
2026-06-07 10:47:04 -0400

    flake dev-shell

--- flake.nix
+++ flake.nix
@@ -67,6 +67,20 @@
         }
         // x86-linux-pkgs
         // linux-pkgs;
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            # Repository Updates (used in update.sh)
+            nix-update # Automatically updates package version and hashes
+            jq # JSON parser for version lookup in updates
+            curl # Fetching API/release information
+
+            # Scripts and Workflows
+            shellcheck # Bash script linter for update.sh
+          ];
+
+          shellHook = "echo 'Activated nix development shell'";
+        };
       }
     )
     // {
