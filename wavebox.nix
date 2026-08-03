0f0a475d5242f5fd49c360b5f3211ba7d47b0ed5
jtliang24 <170479425+jtliang24@users.noreply.github.com>
2026-08-03 11:46:05 +0000

    chore: automated package updates

--- wavebox.nix
+++ wavebox.nix
@@ -157,11 +157,11 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "wavebox";
-  version = "150.2.131-2";
+  version = "151.2.141-2";
 
   src = fetchurl {
     url = "https://download.wavebox.app/stable/linux/deb/amd64/wavebox_${finalAttrs.version}_amd64.deb";
-    hash = "sha256-UJlhs95sKwaBZEZT7SRMuYuhxUT/3i3iXGTDRlSdXD4=";
+    hash = "sha256-TiPf5EggvrgXa2bpHpBWCibWGQ4YMXRG3XqAfluYP58=";
   };
 
   nativeBuildInputs = [
