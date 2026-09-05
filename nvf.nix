46e555ae1bbfe87e7c96a238c0a85c7861af4c05
Jiatao Liang <jtliang24@gmail.com>
2026-09-05 15:10:58 -0400

    build(nvf): remove x86_64-darwin from supported systems

--- nvf.nix
+++ nvf.nix
@@ -9,7 +9,6 @@ let
   # for multiple systems. This is preferable to abstraction libraries.
   forEachSystem = nixpkgs.lib.genAttrs [
     "x86_64-linux"
-    "x86_64-darwin"
     "aarch64-linux"
     "aarch64-darwin"
   ];
