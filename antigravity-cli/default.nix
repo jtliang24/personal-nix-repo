8857c086a3aff39d68019cd86de1f916fff1125c
jtliang24 <170479425+jtliang24@users.noreply.github.com>
2026-09-11 12:52:06 +0000

    chore: automated package updates

--- antigravity-cli/default.nix
+++ antigravity-cli/default.nix
@@ -10,22 +10,22 @@ let
   system_dict = {
     x86_64-linux = {
       name = "linux_x64";
-      hash = "sha256:d9bfee1ae6e4329562cb87da1f5fc3c886d18594837e73e25c3aae00a49499b9";
+      hash = "sha256:6a2c53db6c681fc114f9a1e499e7b4771357ab2852242e56acbd43197d4807f9";
     };
     aarch64-linux = {
       name = "linux_arm64";
-      hash = "sha256:0a8e61f6548865029c4238b9310c686b7a733db5864cae619656abdf48090594";
+      hash = "sha256:882d9160972d09a721422baa554999ad969bc9b3fa680e866a981398f937f19f";
     };
     aarch64-darwin = {
       name = "mac_arm64";
-      hash = "sha256:8fee3c120142490f2eddd286ff7d532b3e660c92d17e60a3612cc0ad1122478f";
+      hash = "sha256:b80425e10a7b92f20679eee5df3bb18e3f9154b3653a373fad47e2f72614248e";
     };
   };
   inherit (stdenv.hostPlatform) system;
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "antigravity-cli";
-  version = "1.2.0";
+  version = "1.2.1";
 
   strictDeps = true;
   __structuredAttrs = true;
