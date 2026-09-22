c20304143bac815a71781c156eb58c0cac9177c9
jtliang24 <170479425+jtliang24@users.noreply.github.com>
2026-09-22 16:49:00 +0000

    chore: automated package updates

--- gh-aw.nix
+++ gh-aw.nix
@@ -7,12 +7,12 @@
 }:
 buildGoLatestModule (finalAttrs: {
   pname = "gh-aw";
-  version = "0.88.7";
+  version = "0.89.17";
   src = fetchFromGitHub {
     owner = "github";
     repo = "gh-aw";
     tag = "v${finalAttrs.version}";
-    sha256 = "sha256-j4HjghuM1wHmT7QhVaGdcBHYp/vRTdhcYR3jgSkhnRo=";
+    sha256 = "sha256-pPRrRw/Hq7vSBVWhu/Ip2vr/3+X9UPYOz3+cOyIqvM8=";
   };
 
   vendorHash = "sha256-YydstLwmlQNoE22DAV9zZyWbeXfo0cJmSssRLVkBB/k=";
